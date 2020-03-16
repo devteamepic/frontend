b3abcaba6b2944324c03cfca87d6052aed8c0507
Denis Markitanov <dmarkitanov@gmail.com>
2020-03-16 20:17:43 +0600

    centralized cross

--- src/components/styled/atoms/dragAndDropStyled.js
+++ src/components/styled/atoms/dragAndDropStyled.js
@@ -6,6 +6,10 @@ width: 400px;
 height: 600px;
 border: 5px dotted black;
 border-radius: 20px;
+text-align: center;
+font-size: 50px;
+verticatl-align: middle;
+line-height: 600px;
 `
 
 export default DragAndDropStyled
