281974c10e989668336de54ae716d478dce70a7c
Denis Markitanov <dmarkitanov@gmail.com>
2020-04-14 10:06:04 +0600

    tested

--- src/components/core/test.js
+++ src/components/core/test.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { connect } from 'react-redux'
 import '../../App.css'
 import ProfItem from '../core/molecules/ProfItem/ProfItem'
+import FileItem from '../core/molecules/FileItem/FileItem'
 import DragAndDrop from '../core/atoms/DragAndDrop/DragAndDrop'
 import Input from '../core/atoms/Input/Input'
 import CheckboxMessage from '../core/molecules/CheckboxMessage/CheckboxMessage'
@@ -19,6 +20,44 @@ const Test = ({ files, concern, dispatch, ...props }) => {
           <List
             color = 'steel'
           >
+            <FileItem
+              fileObject = {{
+                name: 'asdf',
+                size: 'asdf'
+              }}
+            />
+            <FileItem
+              fileObject = {{
+                name: 'asdf',
+                size: 'asdf'
+              }}
+            />
+            <FileItem
+              fileObject = {{
+                name: 'asdf',
+                size: 'asdf'
+              }}
+            />
+            <FileItem
+              fileObject = {{
+                name: 'asdf',
+                size: 'asdf'
+              }}
+            />
+            <FileItem
+              fileObject = {{
+                name: 'asdf',
+                size: 'asdf'
+              }}
+            />
+          </List>
+          <div style={{ height: '100px' }}>
+          </div>
+          <List
+            color = 'denim'
+          >
+            <ProfItem/>
+            <ProfItem/>
             <ProfItem/>
             <ProfItem/>
             <ProfItem/>
