3c9c4a0e60011c8621f37f3b8082327241aff2fb
paydaylight <maxat235@gmail.com>
2020-04-20 12:23:17 +0600

    added default redirect

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import LoginPage from './components/core/pages/LoginPage/LoginPage'
 import RegisterPage from './components/core/pages/RegisterPage/RegisterPage'
 import HomePage from './components/core/pages/HomePage/HomePage'
 import PrivateRoute from './components/core/atoms/PrivateRoute/PrivateRoute'
-import { Route, Switch } from 'react-router-dom'
+import { Route, Switch, Redirect } from 'react-router-dom'
 
 function App(props) {
   return (
@@ -17,6 +17,7 @@ function App(props) {
             window.location.href = 'https://en.wikipedia.org/wiki/Open_source'
             return null
           }}/>
+          <Redirect exact from="/" to="/home" />
           <Route path='/arxiv' component={() => {
             window.location.href = 'https://arxiv.org'
             return null
