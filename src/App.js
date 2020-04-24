3185aba125da29180ac770399c94ac5696002ad7
paydaylight <maxat235@gmail.com>
2020-04-24 16:38:15 +0600

    the only way i was able to run react code inside rails

--- src/App.js
+++ src/App.js
@@ -1,34 +1,47 @@
 import React from 'react';
 import './App.css';
+import reducers from './redux/reducers'
 import LoginPage from './components/core/pages/LoginPage/LoginPage'
 import RegisterPage from './components/core/pages/RegisterPage/RegisterPage'
 import HomePage from './components/core/pages/HomePage/HomePage'
 import PrivateRoute from './components/core/atoms/PrivateRoute/PrivateRoute'
-import { Route, Switch, Redirect } from 'react-router-dom'
+import { Route, Switch, Redirect, BrowserRouter } from 'react-router-dom'
+import {applyMiddleware, createStore} from "redux";
+import {Provider} from 'react-redux'
+
+const createStoreMiddleware = applyMiddleware()(createStore)
 
 function App(props) {
   return (
-      <div className="App">
-        <Switch>
-          <Route exact path='/login' component={LoginPage}/>
-          <Route exact path='/register' component={RegisterPage}/>
-          <PrivateRoute exact path='/home' component={HomePage}/>
-          <Route path='/openSourceWiki' component={() => {
-            window.location.href = 'https://en.wikipedia.org/wiki/Open_source'
-            return null
-          }}/>
-          <Redirect exact from="/" to="/home" />
-          <Route path='/arxiv' component={() => {
-            window.location.href = 'https://arxiv.org'
-            return null
-          }}/>
-          <Route path='/github' component={() => {
-            window.location.href = 'https://github.com/devteamepic'
-            return null
-          }}/>
-        </Switch>
-      </div>
+      <Provider store={createStoreMiddleware(reducers)}>
+        <BrowserRouter>
+          <div className="App">
+
+            <Switch>
+              <Route exact path='/login' component={LoginPage}/>
+              <Route exact path='/register' component={RegisterPage}/>
+              <PrivateRoute exact path='/home' component={HomePage}/>
+              <Route path='/openSourceWiki' component={() => {
+                window.location.href = 'https://en.wikipedia.org/wiki/Open_source'
+                return null
+              }}/>
+              <Redirect exact from="/" to="/home" />
+              <Route path='/arxiv' component={() => {
+                window.location.href = 'https://arxiv.org'
+                return null
+              }}/>
+              <Route path='/github' component={() => {
+                window.location.href = 'https://github.com/devteamepic'
+                return null
+              }}/>
+            </Switch>
+
+          </div>
+        </BrowserRouter>
+      </Provider>
+
   );
 }
 
 export default App
+
