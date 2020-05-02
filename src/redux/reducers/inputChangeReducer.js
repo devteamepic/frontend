fd03588fd57ff944d891d2f85ee0b5d3922053eb
Denis Markitanov <dmarkitanov@gmail.com>
2020-05-02 21:19:54 +0600

    refactored input change

--- src/redux/reducers/inputChangeReducer.js
+++ src/redux/reducers/inputChangeReducer.js
@@ -1,24 +1,23 @@
 const initialState = {
-    email: '',
-    password: ''
+  email: '',
+  password: ''
 }
 
 const loginInputChange = (state = initialState, action) => {
-    console.log(action)
-    switch(action.type) {
-        case 'EMAIL_CHANGE':
-            return {
-                ...state,
-                email: action.payload
-            }
-        case 'PASSWORD_CHANGE':
-            return {
-                ...state,
-                password: action.payload
-            }
-        default:
-            return state
-    }
+  switch(action.type) {
+    case 'EMAIL_CHANGE':
+      return {
+        ...state,
+        email: action.payload
+      }
+    case 'PASSWORD_CHANGE':
+      return {
+        ...state,
+        password: action.payload
+      }
+    default:
+      return state
+  }
 }
 
 export default loginInputChange
