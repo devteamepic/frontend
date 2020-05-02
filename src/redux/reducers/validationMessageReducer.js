0299d3e30a6b95f4ccebd0eda8e3287405e706ed
Denis Markitanov <dmarkitanov@gmail.com>
2020-05-02 21:28:37 +0600

    redirected validation message reduced

--- src/redux/reducers/validationMessageReducer.js
+++ src/redux/reducers/validationMessageReducer.js
@@ -1,32 +1,32 @@
 const validationErrorMessage = (state = '', action) => {
-    switch(action.type) {
-        case 'EMAIL_ERROR':
-            return {
-                ...state,
-                emailError: action.payload
-            }
-        case 'PASSWORD_ERROR':
-            return {
-                ...state,
-                passwordError: action.payload
-            }
-        case 'MATCH_PASSWORD_ERROR':
-            return {
-                ...state,
-                matchPasswordError: action.payload
-            }
-        case 'REMOVE_ERROR_MESSAGE':
-            delete state[action.payload]
-            return {
-                ...state
-            }
-        case 'LOGIN_ERROR':
-            return {
-                loginError: action.payload
-            }
-        default:
-            return state
-    }
+  switch(action.type) {
+    case 'EMAIL_ERROR':
+      return {
+        ...state,
+        emailError: action.payload
+      }
+    case 'PASSWORD_ERROR':
+      return {
+        ...state,
+        passwordError: action.payload
+      }
+    case 'MATCH_PASSWORD_ERROR':
+      return {
+        ...state,
+        matchPasswordError: action.payload
+      }
+    case 'REMOVE_ERROR_MESSAGE':
+      const index = state.indexOf(action.payload)
+      let newArray = [...state]
+      newArray.splice(index, 1)
+      return newArray
+    case 'LOGIN_ERROR':
+      return {
+        loginError: action.payload
+      }
+    default:
+      return state
+  }
 }
 
 export default validationErrorMessage
