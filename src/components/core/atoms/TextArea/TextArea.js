28fbe401f0b3d6103a6a1a3ff0fb13d8cc946e60
Denis Markitanov <dmarkitanov@gmail.com>
2020-05-06 17:51:08 +0600

    added the counter of chars

--- src/components/core/atoms/TextArea/TextArea.js
+++ src/components/core/atoms/TextArea/TextArea.js
@@ -1,23 +1,37 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import TextAreaStyled from '../../../styled/atoms/textAreaStyled'
+import colorScheme from '../../../../misc/colorScheme'
 
 const TextArea = ({ height, width, ...props }) => {
   const [size] = useState({ height, width })
     const [numOfChars, setNumOfChars] = useState(1000)
 
     const handleKeyPress = (e) => {
-        e.preventDefault()
+      e.preventDefault()
 
-        setNumOfChars(numOfChars - 1)
-        console.log(numOfChars)
+      setNumOfChars(1000 - e.target.value.length)
     }
 
   return (
+    <div>
     <TextAreaStyled
       size = { size }
       maxlength = '1000'
-      onKeyUp = { handleKeyPress }
+      onChange = { handleKeyPress }
     />
+      <input
+        style = {{
+          width: size.width,
+          textAlign: 'right',
+          border: '0',
+          backgroundColor: colorScheme.marigold,
+          color: 'black',
+          padding: '0'
+        }}
+        disabled
+        value={ numOfChars }
+      />
+    </div>
   )
 }
 
