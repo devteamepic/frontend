39b8e116a0b1c793224dc17326266587e6d156fa
Denis Markitanov <dmarkitanov@gmail.com>
2020-05-07 12:17:12 +0600

    made extended grid for submission item

--- src/components/core/organisms/SubmissionItem/SubmissionItem.js
+++ src/components/core/organisms/SubmissionItem/SubmissionItem.js
@@ -5,49 +5,82 @@ import Icon from '../../atoms/Icon/Icon'
 import colorScheme from '../../../../misc/colorScheme'
 
 const SubmissionItem = () => {
-    const [header] = useState([
-        { component: 'text', size: 'large', textValue: '1st submission details' },
-    ])
-    const [text] = useState([
-        { component: 'text', size: 'small', textValue: 'Submitted on 02.04.2020' },
-    ])
+  const [isUpsideDown, setIsUpsideDown] = useState(true)
+  const [display, setDisplay] = useState('')
+  const [header] = useState([
+    { component: 'text', size: 'large', textValue: '1st submission details' },
+  ])
+  const [text] = useState([
+    { component: 'text', size: 'small', textValue: 'Submitted on 02.04.2020' },
+  ])
 
-    const handleClick = () => {
-        console.log('a')
-    }
+  const handleClick = () => {
+    setIsUpsideDown(!isUpsideDown)
+    isUpsideDown ? setDisplay('') : setDisplay('display: none;')
+  }
 
-    return (
-        <SubmissionItemStyled>
-          <TextViewer
-            childrenData = { header }
-            notDescription = { true }
-            additionalStyles = { 'color: ' + colorScheme.marigold + '; text-align: left;' }
-          />
-          <TextViewer
-            childrenData = { text }
-            notDescription = { true }
-            additionalStyles = { 'text-align: left;' }
-          />
-          <button
-            onClick = { handleClick }
-            style = {{
-                outline: 'none',
-                border: 'none',
-                background: 'none',
-                width: '50px',
-                height: 'fit-content',
-                padding: '0',
-                transition: '0.5s ease all',
-                gridRow: '1/3',
-            }}
+  return (
+    <SubmissionItemStyled
+      shouldExpand = { isUpsideDown }
+    >
+      <TextViewer
+        childrenData = { header }
+        notDescription = { true }
+        additionalStyles = { 'color: ' + colorScheme.marigold + '; text-align: left;' }
+      />
+      <TextViewer
+        childrenData = { text }
+        notDescription = { true }
+        additionalStyles = { 'text-align: left;' + display }
+      />
+      <button
+        onClick = { handleClick }
+        style = {{
+            outline: 'none',
+            border: 'none',
+            background: 'none',
+            width: '50px',
+            height: isUpsideDown && '50px',
+            padding: '0',
+            transition: '0.5s ease all',
+            gridRow: '1/3',
+        }}
+      >
+        <Icon
+          degree = { isUpsideDown ? '180' : '0' }
+          widthParam = '50px'
+          icon = 'v'
+        />
+      </button>
+      { isUpsideDown &&
+        <div
+          style = {{
+              gridColumn: '1/3',
+              width: '100%',
+              height: '100%',
+              gridRow: '3/-1',
+              display: 'grid',
+              gridTemplateColumns: '40% auto',
+              gridTemplateRows: 'auto auto',
+              padding: '0 10px 10px 0',
+          }}
+        >
+          <div
+            style = {{ width: '100%', height: '100%', backgroundColor: 'red', gridRow: '1/3' }}
           >
-            <Icon
-              widthParam = '50px'
-              icon = 'v'
-            />
-          </button>
-        </SubmissionItemStyled>
-    )
+          </div>
+          <div
+            style = {{ width: '100%', height: '100%', backgroundColor: 'green' }}
+          >
+          </div>
+          <div
+            style = {{ width: '100%', height: '100%', backgroundColor: 'blue' }}
+          >
+          </div>
+        </div>
+      }
+    </SubmissionItemStyled>
+  )
 }
 
 export default SubmissionItem
--- src/components/styled/organisms/submissionItemStyled.js
+++ src/components/styled/organisms/submissionItemStyled.js
@@ -1,6 +1,11 @@
 import styled from 'styled-components'
 import colorScheme from '../../../misc/colorScheme'
 
+const additionalStyles = `
+  height: 700px;
+  grid-template-rows: 50px 50px auto;
+`
+
 const SubmissionItemStyled = styled.li`
 height: fit-content;
 width: 100%;
@@ -12,6 +17,7 @@ display: grid;
 grid-template-columns: 90% auto;
 grid-template-rows: auto auto;
 grid-auto-flow: column;
+${ props => props.shouldExpand && additionalStyles }
 `
 
 export default SubmissionItemStyled
