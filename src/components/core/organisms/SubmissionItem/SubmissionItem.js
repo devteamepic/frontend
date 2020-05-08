96f7e34746c5f3b0e65a975b021486fd9b823072
Denis Markitanov <dmarkitanov@gmail.com>
2020-05-08 22:06:52 +0600

    added ordinal suffix

--- src/components/core/organisms/SubmissionItem/SubmissionItem.js
+++ src/components/core/organisms/SubmissionItem/SubmissionItem.js
@@ -9,10 +9,25 @@ import ProfItem from '../ProfItem/ProfItem'
 import colorScheme from '../../../../misc/colorScheme'
 
 
-const SubmissionItem = () => {
+const SubmissionItem = ({ number, ...props }) => {
+  const ordinalSuffix = (i) => {
+    var j = i % 10,
+        k = i % 100;
+    if (j === 1 && k !== 11) {
+      return i + "st";
+    }
+    if (j === 2 && k !== 12) {
+      return i + "nd";
+    }
+    if (j === 3 && k !== 13) {
+      return i + "rd";
+    }
+    return i + "th";
+  }
+
   const [isUpsideDown, setIsUpsideDown] = useState(false)
   const [header] = useState([
-    { component: 'text', size: 'large', textValue: '1st submission details' },
+    { component: 'text', size: 'large', textValue: ordinalSuffix(number + 1) + ' submission details' },
   ])
   const [text] = useState([
     { component: 'text', size: 'small', textValue: 'Submitted on 02.04.2020' },
