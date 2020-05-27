f91f8c96f7e7728a70039c4f96b2c1ddf780958a
Denis Markitanov <dmarkitanov@gmail.com>
2020-05-27 09:08:35 +0600

    OOOOHHHHHHHHH HAHAAAAA

--- src/components/core/organisms/SubmissionItem/SubmissionItem.js
+++ src/components/core/organisms/SubmissionItem/SubmissionItem.js
@@ -45,7 +45,7 @@ const SubmissionItem = ({ number, abstract, documents, id, ...props }) => {
     { component: 'text', size: 'large', textValue: ordinalSuffix(number + 1) + ' submission details' },
   ])
   const [text] = useState([
-    { component: 'text', size: 'small', textValue: 'Submitted on 02.04.2020' },
+    { component: 'text', size: 'small', textValue: 'Submitted on 27.05.2020' },
   ])
   const [profId] = useState(id)
 
